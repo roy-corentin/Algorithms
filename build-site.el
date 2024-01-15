d4db834e38c4df8ac7d45566c8e34e418f30a92e
roy-corentin <corentin.roy@epitech.eu>
2024-01-15 15:26:23 +0100

    [ADD] WIP github page with emacs

---
+++ build-site.el
@@ -0,0 +1,45 @@
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+;; Load the publishing system
+(require 'ox-publish)
+
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      '(("orgfiles"
+         :recursive t
+         :base-directory "./content"
+         :publishing-function org-html-publish-to-html ; Changed from 'org-html-publish-to-html'
+         :publishing-directory "./public"
+         :with-author nil
+         :with-creator t
+         :with-toc t
+         :section-numbers nil
+         :time-stamp-file nil)
+        ("images"
+         :base-directory "./img"
+         :base-extension "png\\|jpg\\|svg\\|gif"
+         :publishing-directory "./public/img"
+         :publishing-function org-publish-attachment)
+        ("org-site:main" :components("orgfiles" "images"))))
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete!")
